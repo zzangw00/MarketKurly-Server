6051153615abe456bf4ede5ae3e56be451d45c4c
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2021-03-14 20:22:38 +0900

    modify : setting files

--- config/database.js
+++ config/database.js
@@ -1,15 +1,24 @@
 const mysql = require('mysql2/promise');
-const {logger} = require('./winston');
-
-// TODO: 본인의 DB 계정 입력
-const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '3306',
-    password: '',
-    database: ''
-});
+const { logger } = require('./winston');
+let pool;
+if (process.env.NODE_ENV == 'development') {
+    pool = mysql.createPool({
+        host: 'mywebserver.cvifdvvqbxad.ap-northeast-2.rds.amazonaws.com',
+        user: 'user',
+        port: '3306',
+        password: 'ckddn40852!',
+        database: 'Kurly-dev',
+    });
+} else if (process.env.NODE_ENV == 'production') {
+    pool = mysql.createPool({
+        host: 'mywebserver.cvifdvvqbxad.ap-northeast-2.rds.amazonaws.com',
+        user: 'user',
+        port: '3306',
+        password: 'ckddn40852!',
+        database: 'Kurly-prod',
+    });
+}
 
 module.exports = {
-    pool: pool
+    pool: pool,
 };
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('./config/express');
-const {logger} = require('./config/winston');
-
-const port = 3000;
+const { logger } = require('./config/winston');
+const app = express();
+let port;
+if (process.env.NODE_ENV == 'development') {
+    port = 3000;
+} else if (process.env.NODE_ENV == 'production') {
+    port = 3001;
+}
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
